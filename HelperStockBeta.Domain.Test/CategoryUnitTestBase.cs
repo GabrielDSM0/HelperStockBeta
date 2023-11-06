b5c9eb4bdd1c5d0cb564dbf231c2d119357876ac
Gabriel <Gabriel@GABRIEL-NOTEBOO>
2023-11-06 00:08:16 -0300

    Adicionar arquivos de projeto.

---
+++ HelperStockBeta.Domain.Test/CategoryUnitTestBase.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using HelperStockBeta.Domain.Entities;
+namespace HelperStockBeta.Domain.Test;
+
+public class CategoryUnitTestBase
+{
+    #region Testes positivos
+    [Fact(DisplayName = "Category name is not null.")]
+    public void CreateCategory_WithParameters_ResultValid()
+    {
+        Action action = () => new Category(1, "Category Test");
+        action.Should()
+            .NotThrow<HelperStockBeta.Domain.Validation.DomainExceptionValidation>();
+    }
+    [Fact(DisplayName = "Category no present id parameter.")]
+    public void CreateCategory_IdParameterLess_ResultValid()
+    {
+        Action action = () => new Category("Category Test");
+        action.Should()
+            .NotThrow<HelperStockBeta.Domain.Validation.DomainExceptionValidation>();
+    }
+    #endregion
+
+    #region Testes negativos
+    [Fact(DisplayName = "Id negative exception.")]
+    public void CreateCategory_NegativeParameterId_ResultException()
+    {
+        Action action = () => new Category(-1, "Category Test");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Identification is positive values!");
+    }
+    [Fact(DisplayName = "Category name is null.")]
+    public void CreateCategory_NameIsNull_ResultException()
+    {
+         Action action = () => new Category(1, null);
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid name. Name is required!");
+    }
+    [Fact(DisplayName = "Category short name.")]
+    public void CreateCategory_NameIsShort_ResultValid()
+    {
+        Action action = () => new Category(1, "CA");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Name is minimum 3 charecters");
+    }
+    #endregion
+}
---
+++ HelperStockBeta.Domain.Test/ProductUnitTestBase.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using HelperStockBeta.Domain.Entities;
+namespace HelperStockBeta.Domain.Test;
+
+public class ProductUnitTestBase
+{
+    #region Testes positivos
+    [Fact(DisplayName = "Product Id is not null.")]
+    public void CreateProduct_IdParameterLess_ResultValid()
+    {
+        Action action = () => new Product(1, "Product Test", "Teste", 0, 0, "img");
+        action.Should()
+            .NotThrow<HelperStockBeta.Domain.Validation.DomainExceptionValidation>();
+    }
+    [Fact(DisplayName = "Product name is not null.")]
+    public void CreateProduct_WithParameters_ResultValid()
+    {
+        Action action = () => new Product("Product Test", "Teste", 0, 0, "img");
+        action.Should()
+            .NotThrow<HelperStockBeta.Domain.Validation.DomainExceptionValidation>();
+    }
+
+    #endregion
+
+    #region Testes negativos
+    [Fact(DisplayName = "Id negative exception.")]
+    public void CreateProduct_NegativeParameterId_ResultException()
+    {
+        Action action = () => new Product(-1, "Product Name", "Product Description", 10, 1, "imagem1.png");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid negative values for id.");
+    }
+    [Fact(DisplayName = "Name is null")]
+    public void CreateProduct_NameIsNull_ResultException()
+    {
+        Action action = () => new Product(1, null, "Product Description", 10, 1, "imagem1.png");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid name, name is required.");
+    }
+    [Fact(DisplayName = "Product short name.")]
+    public void CreateProduct_NameIsShort_ResultValid()
+    {
+        Action action = () => new Product(1, "CA", "Product Description", 10, 1, "imagem1.png");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid short names, minimum 3 characteres.");
+    }
+    [Fact(DisplayName = "Description is null")]
+    public void CreateProduct_DescriptionIsNull_ResultException()
+    {
+        Action action = () => new Product(2, "Name Product", null, 10, 1, "imagem1.png");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid description, description is required.");
+    }
+    [Fact(DisplayName = "Product short description.")]
+    public void CreateProduct_DescriptionIsShort_ResultValid()
+    {
+        Action action = () => new Product(1, "Product Name", "De", 10, 1, "imagem1.png");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid short descriptions, minimum 5 characters.");
+    }
+    [Fact(DisplayName = "Product negative price.")]
+    public void CreateProduct_PriceIsNegative_ResultValid()
+    {
+        Action action = () => new Product(1, "Product Name", "Product Description", -5, 1, "imagem1.png");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid negative values for price.");
+    }
+    [Fact(DisplayName = "Product negative stock.")]
+    public void CreateProduct_StockIsNegative_ResultValid()
+    {
+        Action action = () => new Product(1, "Product Name", "Product Description", 10, -1, "imagem1.png");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid negative values for stock.");
+    }
+    [Fact(DisplayName = "Product invalid image.")]
+    public void CreateProduct_InvalidImage_ResultValid()
+    {
+        Action action = () => new Product(1, "Product Name", "Product Description", 10, 1, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        action.Should()
+            .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid long URL, maximum 250 characteres.");
+    }
+    #endregion
+}
